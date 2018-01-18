58d9d72daf3e360cc16abd0f53eeb6e39cbeda0e
tscand <tscand@tscands-MacBook-Pro.local>
2018-01-18 16:15:55 -0500

    updateinvite

---
+++ Request/request.js
@@ -0,0 +1,17 @@
+function validation ()
+
+{
+if(document.form[""][""].value==null ||
+   document.form[""].value=="")
+}
+{
+  alert("Please fill out all fields");
+    return false;
+
+}
+.
+.
+.
+
+return true;
+}
